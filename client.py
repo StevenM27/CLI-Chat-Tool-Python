ce465a2ff473f8366fff2f6431284b105e3bc080
Steven Martin <steven@Stevens-MBP.fios-router.home>
2022-12-27 23:00:49 -0800

    Add basic client and server functionalities. Still bugs present

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import json
 
 host = "127.0.0.1"
 port = 12345
@@ -10,13 +11,24 @@ in_chat_room = False
 c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 c.connect((host, port))
 
+c.send(name.encode())
+
 # Thread function for receiving messages
 def receive():
-    pass
+    while in_chat_room:
+        message = c.recv(1024).decode()
+        print(message)
 
 # Thread function for sending messages
 def send():
-    pass
+    while True:
+        message = input('')
+        if message.lower() == "quit":
+            break
+        message = "{}: {}".format(name, message)
+        c.send(message.encode())
+
+    in_chat_room = False
 
 tr = threading.Thread(target=receive)
 tr.start()
@@ -32,12 +44,11 @@ while True:
         print("1. List all chat rooms")
         print("2. Create a new chat room")
         print("3. Join an existing chat room")
-
         choice = input("Enter your option: ")
 
-        if choice == 1:
+        if choice == "1":
             c.send("/option 1".encode())
-            data = c.recv(1024)
+            data = json.loads(c.recv(1024).decode())
             if not data:
                 print("No chat rooms to show!")
                 continue
@@ -48,14 +59,40 @@ while True:
                 
                 continue
 
-        elif choice == 2:
+        elif choice == "2":
             c.send("/option 2".encode())
-            chat_room_number = input("Enter new chat room number: ")
-            data = c.recv(1024)
+            chat_room_id = input("Enter new chat room ID: ")
+            data = json.loads(c.recv(1024).decode())
             if data:
+                if chat_room_id in data.keys():
+                    print("Chat room number", chat_room_id, "already exists.")
+                    c.send("/cancel".encode())
+                    continue
+            
+            chat_room_name = input("Enter new chat room name: ")
 
-        elif choice == 3:
-            pass
+            c.send("/create".encode())
+            c.send(chat_room_id.encode())
+            c.send(chat_room_name.encode())
+
+            in_chat_room = True
+            continue
+
+        elif choice == "3":
+            c.send("/option 3")
+            chat_room_id = input("Enter chat room ID: ")
+            data = json.loads(c.recv(1024).decode())
+            if data:
+                if chat_room_id in data.keys():
+                    c.send(chat_room_id.encode())
+                    response = c.recv(1024).decode()
+                    print(str(response))
+                    continue
+            
+            c.send("/cancel".encode())
+            print("Chat room", chat_room_id, "does not exist.")
+            continue
+
         else:
             print("Invalid response!")
             continue
--- server.py
+++ server.py
@@ -1,55 +1,82 @@
 import socket
-
-from _thread import *
 import threading
+import json
 
 host = "127.0.0.1"
 port = 12345
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+s.listen()
 
 lock = threading.Lock()
 
-chat_rooms = {}
-chat_rooms[101] = []
+client_names = {}
+chat_room_clients = {}
+chat_room_names = {}
 
-#thread function
-def threaded(c):
+
+def send_message(c, id, message):
+    for client in chat_room_clients[id]:
+        message = str(client_names[c]) + ": " + str(message)
+        client.send(str(message).encode())
+
+
+def client_handler(c):
+    lock.release()
     while True:
+        client_response = ""
+        client_response = str(c.recv(1024).decode())
 
-        data = c.recv(1024)
-        if not data:
-            print("Bye")
+        if client_response == "/option 1":
+            # Send all chat room data
+            c.send(json.dumps(chat_room_names, indent=4).encode())
+            continue
+        elif client_response == "/option 2":
+            # Create a new chat room
+            c.send(json.dumps(chat_room_names, indent=4).encode())
+            client_response = str(c.recv(1024).decode())
 
-            lock.release()
-            break
-    
-        data = data[::-1]
-        c.send(data)
-    
-    c.close()
+            if client_response == "/cancel":
+                continue
+            elif client_response == "/create":
+                chat_room_id = str(c.recv(1024).decode())
+                chat_room_name = str(c.recv(1024).decode())
+
+                chat_room_names[chat_room_id] = chat_room_name
+                chat_room_clients[chat_room_id] = [c]
+                continue
+        elif client_response == "/option 3":
+            # join an existing chat room
+            c.send(json.dumps(chat_room_names, indent=4).encode())
+            client_response = str(c.recv(1024).decode())
+
+            if client_response == "/cancel":
+                continue
+            else:
+                chat_room_clients[client_response].append(c)
+                response = "Connected to chatroom " + str(client_response)
+                c.send(response.encode())
+                continue
+
+        else:
+            for id in chat_room_clients:
+                if c in chat_room_clients[id]:
+                    send_message(c, id, client_response)
+                    break
+            continue
 
 
-def Main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((host, port))
-    print("socket bound to port", port)
+while True:
+    c, addr = s.accept()
 
-    s.listen(5)
-    print("socket is listening")
+    lock.acquire()
 
-    while True:
-        c, addr = s.accept()
+    print("Connected to :", addr[0], ':', addr[1])
+    c_name = str(c.recv(1024).decode())
 
-        lock.acquire()
-        print("Connected to :", addr[0], ':', addr[1])
+    print("Client name is", str(c_name))
+    client_names[c] = c_name
 
-        start_new_thread(threaded, (c,))
-    
-    s.close()
-
-
-if __name__ == "__main__":
-    Main()
-
-
+    thread = threading.Thread(target=client_handler, args=(c,))
+    thread.start()
