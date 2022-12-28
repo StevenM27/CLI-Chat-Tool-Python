59445402edfc41ae206f340cd81ffc41423b2696
Steven Martin <83900183+StevenM27@users.noreply.github.com>
2022-12-28 15:26:58 -0800

    Implement multiple chatroom functionality

--- client.py
+++ client.py
@@ -6,7 +6,10 @@ host = "127.0.0.1"
 port = 12345
 name = input("Enter your name: ")
 
-in_chat_room = False
+lock = threading.Lock()
+
+in_chat_room = threading.Event()
+in_chat_room.clear()
 
 c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 c.connect((host, port))
@@ -14,72 +17,105 @@ c.connect((host, port))
 c.send(name.encode())
 
 # Thread function for receiving messages
-def receive():
-    while in_chat_room:
-        message = c.recv(1024).decode()
-        print(message)
+def receive(event, lock):
+    while True:
+        if event.is_set():
+            message = c.recv(1024).decode()
+            if message == "/quit":
+                lock.acquire()
+                print("You have left the server.")
+                event.clear()
+                message = ""
+                lock.release()
+                continue
+            print(message)
 
 # Thread function for sending messages
-def send():
+def send(event, lock):
     while True:
-        message = input('')
-        if message.lower() == "quit":
-            break
-        message = "{}: {}".format(name, message)
-        c.send(message.encode())
+        if event.is_set():
+            message = input('')
+            if message.lower() == "quit":
+                lock.acquire()
+                event.clear()
+                c.send("/quit".encode())
+                message = ""
+                lock.release()
+                continue
+            c.send(message.encode())
+            message = ""
+            continue
 
-    in_chat_room = False
-
-tr = threading.Thread(target=receive)
+tr = threading.Thread(target=receive, args=(in_chat_room,lock,))
+ts = threading.Thread(target=send, args=(in_chat_room,lock,))
 tr.start()
-ts = threading.Thread(target=send)
 ts.start()
 
 # Main thread loop
 while True:
 
-    if not in_chat_room:
+    if not in_chat_room.is_set():
+
+        lock.acquire()
+
         print("You can create a new chat room or join an existing chat room.")
         print("Choose the number corresponding to the desired action:")
         print("1. List all chat rooms")
         print("2. Create a new chat room")
         print("3. Join an existing chat room")
         choice = input("Enter your option: ")
+        print()
 
         if choice == "1":
             c.send("/option 1".encode())
             data = json.loads(c.recv(1024).decode())
             if not data:
                 print("No chat rooms to show!")
+                print()
+                data = ""
+                lock.release()
                 continue
             else:
                 print("Chat Room #, Chat Room Name")
-                for row in data:
-                    print(str(row.first), str(row.second))
-                
+                for id in data:
+                    print(str(id) + ",",  str(data[id]))
+                print()
+                data = ""
+                lock.release()
                 continue
 
         elif choice == "2":
             c.send("/option 2".encode())
             chat_room_id = input("Enter new chat room ID: ")
+            print()
             data = json.loads(c.recv(1024).decode())
             if data:
                 if chat_room_id in data.keys():
                     print("Chat room number", chat_room_id, "already exists.")
+                    print()
                     c.send("/cancel".encode())
+                    data = ""
+                    lock.release()
                     continue
             
+            data = ""
             chat_room_name = input("Enter new chat room name: ")
+            print()
 
             c.send("/create".encode())
             c.send(chat_room_id.encode())
             c.send(chat_room_name.encode())
 
-            in_chat_room = True
+            server_response = c.recv(1024).decode()
+            print(server_response)
+            print()
+
+            in_chat_room.set()
+            lock.release()
             continue
 
         elif choice == "3":
-            c.send("/option 3")
+            c.send("/option 3".encode())
             chat_room_id = input("Enter chat room ID: ")
             data = json.loads(c.recv(1024).decode())
             if data:
@@ -87,12 +123,19 @@ while True:
                     c.send(chat_room_id.encode())
                     response = c.recv(1024).decode()
                     print(str(response))
+                    print()
+                    in_chat_room.set()
+                    data = ""
+                    lock.release()
                     continue
             
             c.send("/cancel".encode())
             print("Chat room", chat_room_id, "does not exist.")
+            print()
+            lock.release()
             continue
 
         else:
             print("Invalid response!")
+            lock.release()
             continue
--- server.py
+++ server.py
@@ -18,14 +18,18 @@ chat_room_names = {}
 
 def send_message(c, id, message):
     for client in chat_room_clients[id]:
-        message = str(client_names[c]) + ": " + str(message)
-        client.send(str(message).encode())
+        c_message = str(client_names[c]) + ": " + str(message)
+        client.send(str(c_message).encode())
 
 
 def client_handler(c):
+    global lock
+    global client_names
+    global chat_room_clients
+    global chat_room_names
     lock.release()
+
     while True:
-        client_response = ""
         client_response = str(c.recv(1024).decode())
 
         if client_response == "/option 1":
@@ -45,6 +49,10 @@ def client_handler(c):
 
                 chat_room_names[chat_room_id] = chat_room_name
                 chat_room_clients[chat_room_id] = [c]
+
+                response = "Connected to chatroom " + str(chat_room_id)
+                c.send(response.encode())
+
                 continue
         elif client_response == "/option 3":
             # join an existing chat room
@@ -60,10 +68,22 @@ def client_handler(c):
                 continue
 
         else:
+            lock.acquire()
+            
             for id in chat_room_clients:
                 if c in chat_room_clients[id]:
+                    if client_response == "/quit":
+                        client_response = ""
+                        c.send("/quit".encode())
+                        chat_room_clients[id].remove(c)
+                        if len(chat_room_clients[id]) == 0:
+                            chat_room_clients.pop(id, 1)
+                            chat_room_names.pop(id, 1)
+                        break
                     send_message(c, id, client_response)
+                    client_response = ""
                     break
+            lock.release()
             continue
 
 
